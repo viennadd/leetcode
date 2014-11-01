3f183d1e3e7a33313efc1b3869e169626a09b46b
viennadd <viennadd@gmail.com>
2014-11-01 22:08:01 +0800

    Initial Commit

---
+++ leetcode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  leetcode
+//
+//  Created by vienna on 1/11/2014.
+//  Copyright (c) 2014年 vienna. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
