812017bd185292178d70dfcb4fe5c3f017aa1dc8
viennadd <viennadd@users.noreply.github.com>
2014-12-30 17:25:34 +0800

    update insert interval mess answer

--- src/insert_interval.cpp
+++ src/insert_interval.cpp
@@ -2,10 +2,6 @@
 
 using namespace std;
 
-/*
-
-*/
-
 struct Interval {
 	int start;
 	int end;
@@ -13,18 +9,23 @@ struct Interval {
 	Interval(int s, int e) : start(s), end(e) {}
 };
 
-cluass Solution {
+class Solution {
 
 private:
 	/*
 		return the position that should be change
+			* can not handle the key less than the first interval, just return -1 
 	*/
 	int search(const vector<Interval> &intervals, const int key)
 	{
 		int i = 0;
 		int j = intervals.size() - 1;
 
-		int position = 0;
+		int position = -1;
+
+		/* hanlde the key bigger than last interval */
+		if (key > (intervals.end() - 1)->start)
+			return intervals.size() - 1;
 
 		while (i < j) {
 			int middle = ((j - i) >> 1) + i;
@@ -48,20 +49,59 @@ private:
 
 		return position;
 	}
+
 public:
 	vector<Interval> insert(vector<Interval> &intervals, Interval newInterval) {
+
+		if (intervals.size() == 0) {
+			intervals.push_back(newInterval);
+			return intervals;
+		}
+
  		int position = search(intervals, newInterval.start);
 
-		if (newInterval.start <= intervals[position].end) {
-			intervals[position].end = newInterval.end;
+		/* replace the interval end or insert a new interval */
+		vector<Interval> ret;
+		if (position == -1) {
+			Interval t = newInterval;
+			if (t.end >= intervals[0].start)
+				t.end = newInterval.end > intervals[0].end ? newInterval.end : intervals[0].end;
+			ret.push_back(t);
+			position = 0;
 		}
 		else {
-			intervals.insert(intervals.begin() + position + 1, newInterval);
-			position++;
+			for (int i = 0; i < position; ++i)
+				ret.push_back(intervals[i]);
+
+			if (newInterval.start > intervals[position].end) {
+				ret.push_back(intervals[position]);
+				ret.push_back(newInterval);
+			}
+			else {
+				if (newInterval.end > intervals[position].end) {
+					intervals[position].end = newInterval.end;
+				}
+
+				ret.push_back(intervals[position]);
+
+				
+			}
 		}
 
-		if (1)
+		/* merge intervals */
+		
+		for (auto it = intervals.begin() + position; it != intervals.end(); ++it) {
 
-		return intervals;
+			if (it->start > newInterval.end)
+				ret.push_back(*it);	
+			else if (it->start <= newInterval.end) {
+				Interval t = *ret.rbegin();
+				int end = it->end > newInterval.end ? it->end : newInterval.end;
+				ret.pop_back();
+				ret.push_back(Interval(t.start, end));
+			}
+		}
+
+		return ret;
 	}
 };
